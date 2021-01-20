54c6d4c02697e4c36857600fc23b8b35ef12721d
Vikrant Sharma <iamvikrant1@gmail.com>
2021-01-20 09:16:54 +0530

    Final Update

--- CRUD Application/routes/users.js
+++ CRUD Application/routes/users.js
@@ -3,18 +3,7 @@ import {v4 as uuidv4} from 'uuid';
 
 const router = express.Router();
 
-const users = [
-    {
-        firstname: "Vikrant",
-        secondname: "Sharma",
-        age: 21
-    },
-    {
-        firstname: "Sanya",
-        secondname: "Kumari",
-        age: 20
-    }
-];
+let users = [];
 
 //all routes are starting with /users
 router.get('/',(req, res)=>{
@@ -29,5 +18,36 @@ router.post('/',(req, res)=>{
     res.send(`${user.firstname} added to the database`);
 });
 
+router.get('/:id',(req, res)=>{
+
+    const {id} = req.params;
+
+    const foundUser = users.find((user)=> user.id ===id);
+
+    res.send(foundUser);
+});
+
+router.delete('/:id',(req, res) =>{
+    const {id} = req.params;
+
+    users = users.filter((user)=>user.id !== id);
+
+    res.send(`User with ${id} deleted`);
+});
+
+router.patch('/:id',(req, res) =>{
+    const {id} = req.params;
+    const {firstname, lastname, age} = req.body;
+    const user = users.find((user) => user.id == id);
+
+    if(firstname) user.firstname = firstname;
+    
+    if(lastname) user.lastname = lastname;
+    
+    if(age) user.age = age;
+
+    res.send(`User with ${id} has been updated`);
+    
+});
 
 export default router;
